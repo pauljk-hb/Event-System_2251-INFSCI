071806a40dbf2c464e1b4f0c455725ab75a64ed4
Paul Kamp <115874788+pauljk-hb@users.noreply.github.com>
2024-11-15 11:16:30 -0500

    first View

--- EventManager/events/models.py
+++ EventManager/events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
+    time = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    type = models.CharField(max_length=50)
+    tags = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+
+class EventSignup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='signups')
+    signup_date = models.DateTimeField(auto_now_add=True)
