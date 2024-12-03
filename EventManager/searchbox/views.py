49ae177136300df977c97413475b7a9f0710352e
anupeesay10 <141187396+anupeesay10@users.noreply.github.com>
2024-12-02 20:50:43 -0500

    can search by location

--- EventManager/searchbox/views.py
+++ EventManager/searchbox/views.py
@@ -5,15 +5,16 @@ def event_search(request):
   query = request.GET.get("search", "")
 
 
-  # Here you would typically query the database. For now, let's use a hardcoded list of events.
   events = [
-      {"name": "Chess", "location": "Room 301", "tags":'chess, board game'},
+      {"name": "Chess", "location": "Room 301", "time": '2:30', "tags":'chess, board game'},
       {"name": "Uno", "location": "Room 502", "tags": 'uno, card'},
   ]
 
   # Filter events based on the search query
-  filtered_events = [event for event in events if query.lower() in event["tags"].lower()]
-
+  filtered_events = [
+        event for event in events
+        if query in event["name"].lower() or query in event["location"].lower() or query in event["tags"].lower() 
+    ]
 
   # Pass filtered events to the template
   return render(request, "searchbox/event_search.html", {"events": filtered_events})
