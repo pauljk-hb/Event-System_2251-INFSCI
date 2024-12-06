c88e8d5a984c794b65b28e49f8e0536b7ddd089b
Paul Kamp <115874788+pauljk-hb@users.noreply.github.com>
2024-12-05 22:23:48 -0500

    index site & database

--- EventManager/events/views.py
+++ EventManager/events/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Event
 
 def index(request):
-    return HttpResponse("Hello, world.... You're at the events index.")
+    events = Event.objects.all()
+    context = {'events': events}
+    return render(request, 'events/index.html', context)
