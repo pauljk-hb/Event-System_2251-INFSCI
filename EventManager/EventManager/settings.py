e5fde6a5a35ffe415389dc3ef125ca1523eca2d4
jacobspr23 <79618433+jacobspr23@users.noreply.github.com>
2024-12-07 18:17:21 -0500

    bootstrap added to login and register page

--- EventManager/EventManager/settings.py
+++ EventManager/EventManager/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
